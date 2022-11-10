09dc1dac49fec51c4ac909c78733d84c5f7ab9cf
Tianchi-Liu <liutc@berkeley.edu>
2022-11-09 17:02:39 -0800

    extract MPI to `mpi_khm_dhs14_rgn.csv` in output/data

--- code/MPI/postprocess.py
+++ code/MPI/postprocess.py
@@ -10,9 +10,15 @@ import pandas as pd
 
 # code_path = Path(__file__)
 code_path = Path(r'C:\Users\tianc\OneDrive\Documents\SIG\DISES\code\MPI')
-datafd_path = code_path.parent.parent / 'data' / 'MPI' / 'dta'
+datafd_path = code_path.parent.parent / 'data' / 'MPI' 
+outfd_path = code_path.parent.parent / 'output' / 'data' 
 
-
-df = pd.read_stata(datafd_path / 'khm_dhs14_mpi_rgn1.dta')
-
-df.MPI_1[0]
+# regional MPI based on Cambodia DHS 2014
+mpi_dic = {}
+for i in range(1,20):
+    df = pd.read_stata(datafd_path / 'dta' / f'khm_dhs14_mpi_rgn{i}.dta')
+    mpi = df.MPI_1[0]
+    mpi_dic[i] = mpi
+    
+mpi_df = pd.DataFrame(mpi_dic.items(), columns=['region_code', 'mpi'])
+# mpi_df.to_csv(outfd_path / 'mpi_khm_dhs14_rgn.csv', index=False)
