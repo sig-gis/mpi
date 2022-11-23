17c6cbdb0a359d930dcaa2338793cd3eccce53c5
Tianchi-Liu <liutc@berkeley.edu>
2022-11-23 10:38:30 -0800

    extract cluster MPIs to csv in output/data

--- code/MPI/postprocess.py
+++ code/MPI/postprocess.py
@@ -13,7 +13,7 @@ code_path = Path(r'C:\Users\tianc\OneDrive\Documents\SIG\DISES\code\MPI')
 datafd_path = code_path.parent.parent / 'data' / 'MPI' 
 outfd_path = code_path.parent.parent / 'output' / 'data' 
 
-# regional MPI based on Cambodia DHS 2014
+# %% regional MPI based on Cambodia DHS 2014
 mpi_dic = {}
 for i in range(1,20):
     df = pd.read_stata(datafd_path / 'dta' / f'khm_dhs14_mpi_rgn{i}.dta')
@@ -22,3 +22,16 @@ for i in range(1,20):
     
 mpi_df = pd.DataFrame(mpi_dic.items(), columns=['region_code', 'mpi'])
 # mpi_df.to_csv(outfd_path / 'mpi_khm_dhs14_rgn.csv', index=False)
+
+
+# %% cluster-level MPI based on Cambodia DHS 2014
+spatial_res = 'clust'
+mpi_dic = {}
+for i in range(1,611+1):
+    df = pd.read_stata(
+        datafd_path / 'dta' / f'khm_dhs14_mpi_{spatial_res}{i}.dta')
+    mpi = df.MPI_1[0]
+    mpi_dic[i] = mpi
+    
+mpi_df = pd.DataFrame(mpi_dic.items(), columns=['clust_no', 'mpi'])
+# mpi_df.to_csv(outfd_path / 'mpi_khm_dhs14_clust.csv', index=False)
