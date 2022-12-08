6f562d1ba71d8149d6299c10277f4972745a4a49
Tianchi-Liu <liutc@berkeley.edu>
2022-12-08 14:14:33 -0800

    fix typos in .py; explore SE, CI, and missingness in .ipynb; join cluster-level MPIs, their SE, CI, and missingness to shapefile

--- code/MPI/postprocess.py
+++ code/MPI/postprocess.py
@@ -7,6 +7,7 @@ Created on Wed Nov  9 16:01:01 2022
 
 from pathlib import Path
 import pandas as pd
+import geopandas as gpd
 
 # code_path = Path(__file__)
 code_path = Path(r'C:\Users\tianc\OneDrive\Documents\SIG\DISES\code\MPI')
@@ -26,19 +27,29 @@ mpi_df = pd.DataFrame(mpi_dic.items(), columns=['region_code', 'mpi'])
 
 # %% cluster-level MPI based on Cambodia DHS 2014
 spatial_res = 'clust'
-mpi_ci_df = pd.DataFrame()
+mpi_info_df = pd.DataFrame()
 for i in range(1,611+1):
     df = pd.read_stata(
         datafd_path / 'dta' / f'khm_dhs14_mpi_{spatial_res}{i}.dta')
     row_df = df.loc[[0], ['psu', 'MPI_1_svy', 'MPI_1_SE', \
                           'MPI_1_low95CI', 'MPI_1_upp95CI']]
     row_df['tot_samp_ppl'] = df.shape[0]
-    row_df['ppt_samp_ppl_mis'] = (1 - df.per_sample_1[0]) * 100
-    mpi_ci_df = pd.concat([mpi_ci_df, row_df])
+    row_df['pct_samp_ppl_mis'] = (1 - df.per_sample_1[0]) * 100
+    mpi_info_df = pd.concat([mpi_info_df, row_df])
 
 old_colnames = ['psu', 'MPI_1_svy', 'MPI_1_SE', \
                 'MPI_1_low95CI', 'MPI_1_upp95CI']
 new_colnames = ['clust_no', 'mpi', 'mpi_SE', 'mpi_lo95CI', 'mpi_up95CI']
-mpi_ci_df.rename(columns=dict(zip(old_colnames, new_colnames)), inplace=True)
-# mpi_ci_df.to_csv(outfd_path / 'mpi_khm_dhs14_clust_CI_mis.csv', index=False)
+mpi_info_df.rename(columns=dict(zip(old_colnames, new_colnames)), inplace=True)
+# mpi_info_df.to_csv(outfd_path / 'mpi_khm_dhs14_clust_CI_mis.csv', index=False)
 
+# %%% join the above df to cluster shapefile
+# cluster shapefile
+shp_path = datafd_path.parent/'DHS'/'Cambodia'/'geog'/'KHGE71FL'
+gdf = gpd.read_file(shp_path)
+# attribute join
+mpi_info_gdf = gdf.merge(mpi_info_df, left_on='DHSCLUST', right_on='clust_no')
+# export
+mpi_info_gdf.rename(columns={'tot_samp_ppl': 'tot_ppl',
+                             'pct_samp_ppl_mis': 'pct_pplmis'}, inplace=True)
+# mpi_info_gdf.to_file(outfd_path/'mpi_khm_dhs14_clust_CI_mis')
