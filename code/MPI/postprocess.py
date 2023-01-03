30b08fed74242363e727d460237e409947713981
Tianchi-Liu <liutc@berkeley.edu>
2023-01-03 13:59:30 -0800

    join cluster-level MPIs, their SE, CI, and missingness to shapefile (khm dhs10)

--- code/MPI/postprocess.py
+++ code/MPI/postprocess.py
@@ -8,6 +8,7 @@ Created on Wed Nov  9 16:01:01 2022
 from pathlib import Path
 import pandas as pd
 import geopandas as gpd
+import numpy as np
 
 # code_path = Path(__file__)
 code_path = Path(r'C:\Users\tianc\OneDrive\Documents\SIG\DISES\code\MPI')
@@ -87,3 +88,21 @@ survey = 'khm_dhs10'
 spatial_res = 'clust'
 n_spatial_unit = 611
 mpi_info_df = mpi_info_to_csv(survey, spatial_res, n_spatial_unit)
+
+# %%% join the above df to cluster shapefile
+# cluster shapefile
+shp_path = datafd_path.parent/'DHS'/'Cambodia'/'geog'/'KHGE62FL'
+gdf = gpd.read_file(shp_path)
+# sanity check before joining
+gdf.columns
+(np.sort(gdf.DHSCLUST.unique()) == np.arange(1, n_spatial_unit+1)).all()
+mpi_info_df.shape
+gdf.shape
+# attribute join
+mpi_info_gdf = gdf.merge(mpi_info_df, left_on='DHSCLUST', right_on='clust_no')
+# sanity check after joining
+mpi_info_gdf.shape
+# export
+mpi_info_gdf.rename(columns={'tot_samp_ppl': 'tot_ppl',
+                             'pct_samp_ppl_mis': 'pct_pplmis'}, inplace=True)
+# mpi_info_gdf.to_file(outfd_path/f'mpi_{survey}_clust_CI_mis')
