aec9c3e7b19be75e915d2a90695e52741c2d374e
Tianchi-Liu <liutc@berkeley.edu>
2023-04-12 17:37:36 -0700

    extract khm 14 harmonized cluster MPIs & their SE, CI, missingness from dta to csv

--- code/MPI/postprocess.py
+++ code/MPI/postprocess.py
@@ -67,9 +67,12 @@ def mpi_info_to_csv(survey, spatial_res, n_spatial_unit):
     mpi_info_df = pd.DataFrame()
     for i in range(1,n_spatial_unit+1):
         df = pd.read_stata(
-            datafd_path / survey / f'{survey}_mpi_{spatial_res}{i}.dta')
-        row_df = df.loc[[0], ['psu', 'MPI_1_svy', 'MPI_1_SE', \
-                              'MPI_1_low95CI', 'MPI_1_upp95CI']]
+            datafd_path / survey / f'{"_".join(survey.split("_")[:2])}_mpi_{spatial_res}{i}.dta'
+            )  # delete _cot in khm_dhs14_cot
+        row_df = df.loc[
+            [0], 
+            ['psu', 'MPI_1_svy', 'MPI_1_SE', 'MPI_1_low95CI', 'MPI_1_upp95CI']
+            ]
         row_df['tot_samp_ppl'] = df.shape[0]
         row_df['pct_samp_ppl_mis'] = (1 - df.per_sample_1[0]) * 100
         mpi_info_df = pd.concat([mpi_info_df, row_df])
@@ -77,7 +80,9 @@ def mpi_info_to_csv(survey, spatial_res, n_spatial_unit):
     old_colnames = ['psu', 'MPI_1_svy', 'MPI_1_SE', \
                     'MPI_1_low95CI', 'MPI_1_upp95CI']
     new_colnames = ['clust_no', 'mpi', 'mpi_SE', 'mpi_lo95CI', 'mpi_up95CI']
-    mpi_info_df.rename(columns=dict(zip(old_colnames, new_colnames)), inplace=True)
+    mpi_info_df.rename(
+        columns=dict(zip(old_colnames, new_colnames)), inplace=True
+        )
     mpi_info_df.to_csv(outfd_path / f'mpi_{survey}_{spatial_res}_CI_mis.csv', 
                        index=False)
     return mpi_info_df
@@ -106,3 +111,10 @@ mpi_info_gdf.shape
 mpi_info_gdf.rename(columns={'tot_samp_ppl': 'tot_ppl',
                              'pct_samp_ppl_mis': 'pct_pplmis'}, inplace=True)
 # mpi_info_gdf.to_file(outfd_path/f'mpi_{survey}_clust_CI_mis')
+
+
+# %%% harmonized cluster-level MPI based on Cambodia DHS 2014
+survey = 'khm_dhs14_cot'
+spatial_res = 'clust'
+n_spatial_unit = 611
+mpi_info_df = mpi_info_to_csv(survey, spatial_res, n_spatial_unit)
