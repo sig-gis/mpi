f2ee0cf2a2c2183f58671403ebc06515b9d3657a
Tianchi-Liu <liutc@berkeley.edu>
2024-05-22 07:57:38 -0400

    Postprocess outputs of `khm_dhs10_cluster_cot_nowall.do`
    Compile cluster-level data to one CSV table and join it to cluster point shapefile

--- code/MPI/postprocess.py
+++ code/MPI/postprocess.py
@@ -211,3 +211,29 @@ mpi_info_gdf.shape
 mpi_info_gdf.rename(columns={'tot_samp_ppl': 'tot_ppl',
                              'pct_samp_ppl_mis': 'pct_pplmis'}, inplace=True)
 # mpi_info_gdf.to_file(outfd_path/f'mpi_{survey}_clust_CI_mis')
+
+
+# %%% harmonized cluster-level MPI based on Cambodia DHS 2010 excluding wall
+survey = 'khm_dhs10_cot_nowall'
+spatial_res = 'clust'
+n_spatial_unit = 611
+mpi_info_df = mpi_info_to_csv(survey, spatial_res, n_spatial_unit)
+
+# %%%% join the above df to cluster shapefile
+# cluster shapefile
+shp_path = datafd_path.parent/'DHS'/'Cambodia'/'geog'/'KHGE62FL'
+gdf = gpd.read_file(shp_path)
+# sanity check before joining
+gdf.columns
+(np.sort(gdf.DHSCLUST.unique()) == np.arange(1, n_spatial_unit+1)).all()
+mpi_info_df.shape
+gdf.shape
+# attribute join
+mpi_info_gdf = gdf.merge(mpi_info_df, left_on='DHSCLUST', right_on='clust_no')
+# sanity check after joining
+mpi_info_gdf.shape
+# export
+mpi_info_gdf.rename(columns={'tot_samp_ppl': 'tot_ppl',
+                             'pct_samp_ppl_mis': 'pct_pplmis'}, inplace=True)
+# mpi_info_gdf.to_file(outfd_path/f'mpi_{survey}_clust_CI_mis')
+
