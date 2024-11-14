1071001682b5d77472bee96c77e452e3f1528a7a
Tianchi-Liu <liutc@berkeley.edu>
2024-11-14 18:38:07 -0500

    Compile cluster MPIs into one CSV per survey year
    Add uncensored ones to current ones (censored based on cutoff .33)

--- code/MPI/postprocess_summarize_mpi.py
+++ code/MPI/postprocess_summarize_mpi.py
@@ -16,14 +16,18 @@ def mpi_info_to_csv(
     '''
     
     mpi_info_df = pd.DataFrame()
-    for i in range(1, 1000):  # n_spatial_unit + 1):
+    for i in range(1, n_spatial_unit + 1):
         try:
             df = pd.read_stata(
                 datafd_path / f'{survey}_mpi_{spatial_res}{i}.dta'
             )
             row_df = df.loc[
                 [0], 
-                ['psu', 'area', 'region', 'MPI_1_svy', 'MPI_1_SE', 'MPI_1_low95CI', 'MPI_1_upp95CI']
+                [
+                    'psu', 'area', 'region', 
+                    'MPI_0', 'MPI_0_SE', 'MPI_0_low95CI', 'MPI_0_upp95CI', 
+                    'MPI_33', 'MPI_33_SE', 'MPI_33_low95CI', 'MPI_33_upp95CI'
+                ]
             ]
             row_df['tot_samp_ppl'] = df.shape[0]
             row_df['pct_samp_ppl_mis'] = (1 - df.per_sample_1[0]) * 100
@@ -33,9 +37,15 @@ def mpi_info_to_csv(
     assert mpi_info_df.shape[0] == n_spatial_unit
 
     old_colnames = [
-        'psu', 'MPI_1_svy', 'MPI_1_SE', 'MPI_1_low95CI', 'MPI_1_upp95CI'
+        'psu', 
+        'MPI_0', 'MPI_0_SE', 'MPI_0_low95CI', 'MPI_0_upp95CI', 
+        'MPI_33', 'MPI_33_SE', 'MPI_33_low95CI', 'MPI_33_upp95CI'
     ]
-    new_colnames = ['clust_no', 'mpi', 'mpi_SE', 'mpi_lo95CI', 'mpi_up95CI']
+    new_colnames = [
+        'clust_no', 
+        'mpi_0', 'mpi_0_SE', 'mpi_0_loCI', 'mpi_0_upCI',
+        'mpi_3', 'mpi_3_SE', 'mpi_3_loCI', 'mpi_3_upCI'
+    ]
     mpi_info_df.rename(
         columns=dict(zip(old_colnames, new_colnames)), inplace=True
     )
